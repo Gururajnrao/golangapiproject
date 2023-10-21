236d84d0f64412b3139938ff875c1816b29af441
g0n013l <gururaj.nagaraja.rao@walmart.com>
2023-10-21 19:23:43 +0530

    initial version

---
+++ apiproject-test/my-inventory/app.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+)
+
+type App struct {
+	Router *mux.Router
+	DB     *sql.DB
+}
+
+func (app *App) Initialise() error {
+	const DbName = "inventory"
+	const DbUser = "root"
+	connectionString := fmt.Sprintf("%v:%v@tcp(127.0.0.1:3306)/%v", DbUser, DbPassword, DbName)
+	var err error
+	app.DB, err = sql.Open("mysql", connectionString)
+	if err != nil {
+		return err
+	}
+	app.Router = mux.NewRouter().StrictSlash(true) // Ensures that you are able to route a path like /articles to /articles/ with strictslash
+	app.handleRoutes()
+	return nil
+}
+
+func (app *App) Run(address string) {
+	log.Fatal(http.ListenAndServe(address, app.Router))
+}
+
+func sendResponse(w http.ResponseWriter, statusCode int, payload interface{}) { // Type interface helps to allow us to give any input to payload.More details at: https://www.geeksforgeeks.org/interfaces-in-golang/
+	response, _ := json.Marshal(payload)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	w.Write(response)
+}
+
+func sendError(w http.ResponseWriter, statusCode int, err string) {
+	error_message := map[string]string{"error": err}
+	sendResponse(w, statusCode, error_message)
+}
+
+func (app *App) getProducts(w http.ResponseWriter, r *http.Request) {
+	products, err := getProducts(app.DB)
+	if err != nil {
+		sendError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	sendResponse(w, http.StatusOK, products)
+
+}
+
+func (app *App) handleRoutes() {
+	app.Router.HandleFunc("/products", app.getProducts).Methods("GET")
+
+}
